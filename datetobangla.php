81ac88889f26743178b1064e554502b63fe6a08b
aynamohol <aynamohol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-04 18:51:44 +0000

    version 0.2 released of Banglay Dekhao Date
    git-svn-id: https://plugins.svn.wordpress.org/bn-plugin/trunk@620987 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ datetobangla.php
@@ -0,0 +1,32 @@
+<?php
+/*
+	Plugin Name: Banglay Dekhao Date
+	Version: 0.2
+	Plugin URI: http://www.facebook.com/saamraatkhaan
+	Description: This plugin working itself. It is not related with any other plugin. This is plugin converting Strings to Localization version. 
+	Author: Samrat Khan
+	Author URI: http://www.facebook.com/saamraatkhaan
+*/ 
+function getBanglaDate($date){
+$engArray = array(
+ 1,2,3,4,5,6,7,8,9,0,
+ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+ 'am', 'pm', 'th', 'st', 'rd', 'th', 'Sat', 'Sun', 'Mon', 'Tue', 'Tues', 'Wed', 'Thurs', 'Thu', 'Fri', 'Saturday', 'Sunday',
+ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'
+ ); 
+ $bangArray = array(
+ '১','২','৩','৪','৫','৬','৭','৮','৯','০',
+ 'জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'আগস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর',
+ 'সকাল', 'দুপুর', '-এ', '-এ', '-এ', '-এ', 'শনি', 'রবি', 'সোম', 'মঙ্গল', 'মঙ্গল', 'বুধ', 'বৃহস্পতি', 'বৃহস্পতি', 'শুক্র', 'শনিবার', 'রবিবার',
+ 'সোমবার', 'মঙ্গলবার', 'বুধবার', 'বৃহস্পতিবার', 'শুক্রবার'
+ );
+
+ $converted = str_replace($engArray, $bangArray, $date);
+ return $converted;
+}
+add_filter( 'date', 'getBanglaDate' );
+add_filter( 'get_the_time', 'getBanglaDate' );
+add_filter( 'the_date', 'getBanglaDate' );
+add_filter( 'get_comment_date', 'getBanglaDate' );
+add_filter( 'get_comment_time', 'getBanglaDate' );
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+Contributors: skydotint, aynamohol
+Tags: bangla date, banglay dekhao date, date to bangla
+Requires at least: 3.2.1
+Tested up to: 3.2.1
+Stable tag: 1.4
+
+Active the plugin, it will work itself.
+
+== Description ==
+
+A Bangla Plugin which will conert the wordpress theme to Bangla.
+
+= Banglay Dekhao Date's Homepage =
+
+[Visit this plugin's homepage](http://www.skydotint.com/)
+
+== Installation ==
+
+1. Upload the **bn-plugin** folder to your **/wp-content/plugins/** directory or go to Plugins
+2. Activate the plugin through the Plugins menu in WordPress
+
+= Configure BDD =
+* Later *
+
+
+== Frequently Asked Questions ==
+* Later *
+== Screenshots ==
+
+* Later *
+
+== Changelog ==
+
+* Later *
+
+== Upgrade Notice ==
+* Later *
